8819813a34244b243c5b43895db247295bf86137
Semer7 <semer7@gmail.com>
2023-08-29 13:05:43 +0300

    Added test for 80 symbols limit

--- src/test/scala/challenge/Test.scala
+++ src/test/scala/challenge/Test.scala
@@ -11,8 +11,10 @@ class Test extends AnyFunSuite with Matchers {
   val testMultipleLines = "In 1991, while studying computer science at University of Helsinki, Linus Torvalds began a project that later became the Linux kernel.\nHe wrote the program specifically for the hardware he was using and independent of an operating system because he wanted to use the functions of his new PC with an 80386 processor. Development was done on MINIX using the GNU C Compiler."
 
   val exampleLength = 40
+  val doubleLength = 80
   val exampleResult = "In 1991, while studying computer science\nat University of Helsinki, Linus\nTorvalds began a project that later\nbecame the Linux kernel. He wrote the\nprogram specifically for the hardware he\nwas using and independent of an\noperating system because he wanted to\nuse the functions of his new PC with an\n80386 processor. Development was done on\nMINIX using the GNU C Compiler."
   val exampleResultMultiline = "In 1991, while studying computer science\nat University of Helsinki, Linus\nTorvalds began a project that later\nbecame the Linux kernel.\nHe wrote the program specifically for\nthe hardware he was using and\nindependent of an operating system\nbecause he wanted to use the functions\nof his new PC with an 80386 processor.\nDevelopment was done on MINIX using the\nGNU C Compiler."
+  val resultDoubleLength = "In 1991, while studying computer science at University of Helsinki, Linus\nTorvalds began a project that later became the Linux kernel. He wrote the\nprogram specifically for the hardware he was using and independent of an\noperating system because he wanted to use the functions of his new PC with an\n80386 processor. Development was done on MINIX using the GNU C Compiler."
 
   test("Example case") {
       Main.reformat(testOneLine, exampleLength) shouldBe exampleResult
@@ -27,10 +29,14 @@ class Test extends AnyFunSuite with Matchers {
   }
 
   test("Newlines should be preserved") {
-    Main.reformat(testMultipleLines, 40) shouldBe  exampleResultMultiline
+    Main.reformat(testMultipleLines, exampleLength) shouldBe  exampleResultMultiline
   }
 
   test("Text is already formatted should return identical formatted text") {
-    Main.reformat(exampleResult, 40) shouldBe exampleResult
+    Main.reformat(exampleResult, exampleLength) shouldBe exampleResult
+  }
+
+  test("Example case with line limit 80") {
+    Main.reformat(testOneLine, doubleLength) shouldBe resultDoubleLength
   }
 }
